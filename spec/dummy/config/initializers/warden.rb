ac355f446798b0ad14b539c218e8162ffda4a833
halo <github@posteo.org>
2015-03-11 09:16:50 +0100

    Added some comments about things not to forget

--- spec/dummy/config/initializers/warden.rb
+++ spec/dummy/config/initializers/warden.rb
@@ -7,6 +7,16 @@
   def authenticate!
     Rails.logger.debug(progname) { 'Authenticating from username and password...' }
 
+    # Note that at this point you might want to log the end-user IP for the attempted login.
+    # That's up to you to solve, but remember one thing:
+    # If you both have an untrusted OAuth client (iPhone) and a trusted one (Alpha Rails app)
+    # and the login at Alpha is performed using the "Resource Owner Password Credentials Grant"
+    # Then you will get Alphas IP, but not the end-users IP. So you might have to pass on the
+    # end user IP from Alpha via params. But you cannot trust params, since the iPhone Client
+    # is not trusted. Thus, in this particular scenario, you cannot blindly trust params['ip']
+    # but you'd have to work with the "insider" and "outsider" doorkeeper application scope
+    # restrictions much like SSO::Server::Authentications::Passport#ip does.
+
     user = ::User.authenticate params['username'], params['password']
 
     if user
